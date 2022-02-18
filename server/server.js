b9d55eaa0ffa79c02bbf9bc8288e274ae0a268b5
Paul Hüfner <p.huefner03@gmail.com>
2022-02-18 14:38:13 +0100

    Simple server routing and login feature.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
+const { nextTick } = require("process");
 const app = express();
+const db = require("./db.json");
 
 app.use(express.static(path.join(__dirname + "/public")));
+app.use(bodyParser.urlencoded());
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.sendFile(".\\index.html");
+app.post("/login", (req, res) => {
+    var username = req.body.Benutzername;
+    var password = req.body.Passwort;
+    db.user.forEach(element => {
+        console.log(element.name);
+        console.log(element.password);
+        if (element.name == username && element.password == password) {
+            res.redirect("/homepage");
+        }
+    });
+    res.redirect("/");
+});
+
+app.get("/homepage", (req, res) => {
+    res.sendFile(__dirname + "/public/homepage.html");
 });
 
 app.listen(port, () => {
